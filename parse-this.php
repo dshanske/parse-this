f4d0b5cffae3ef2478556f4f8c4e45cb236b63ed
David Shanske <david@shanske.com>
2018-09-18 14:54:14 -0500

    Manual load for MF2 Parser

--- parse-this.php
+++ parse-this.php
@@ -1,9 +1,25 @@
 <?php
+/**
+ * Plugin Name: Parse This
+ * Plugin URI: https://github.com/dshanske/parse-this
+ * Description: 
+ * Version: 1.0
+ * Author: David Shanske
+ * Author URI: https://david.shanske.com
+ * Text Domain: parse-this
+ * Domain Path:  /languages
+ */
+
+
 /* Parse This Load
  */
 
 if ( ! function_exists( 'parse_this_loader' ) ) {
 	function parse_this_loader() {
+
+                if ( ! class_exists( 'Mf2\Parser' ) ) {
+			require_once dirname( __FILE__ ) . '/vendor/mf2/mf2/Mf2/Parser.php';
+		}
 
 		// Global Functions
 		require_once plugin_dir_path( __FILE__ ) . 'functions.php';
@@ -22,5 +38,5 @@ if ( ! function_exists( 'parse_this_loader' ) ) {
 		// Parse This API
 		require_once plugin_dir_path( __FILE__ ) . 'class-parse-this-api.php';
 	}
-	parse_this_loader();
+	add_action( 'plugins_loaded', 'parse_this_loader' );
 }
