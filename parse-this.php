348dff415327d2047256885b14c1de708b72494b
David Shanske <david@shanske.com>
2019-06-15 02:31:18 +0000

    Adjust due change to autoloader

--- parse-this.php
+++ parse-this.php
@@ -18,11 +18,26 @@ if ( ! function_exists( 'parse_this_loader' ) ) {
 	function parse_this_loader() {
 		require_once plugin_dir_path( __FILE__ ) . 'includes/functions.php';
 		// Parse This API
-		require_once plugin_dir_path( __FILE__ ) . 'includes/class-parse-this-api.php';
+		require_once plugin_dir_path( __FILE__ ) . 'includes/class-rest-parse-this.php';
 
 		// MF2 Post
-		require_once plugin_dir_path( __FILE__ ) . 'includes/class-mf2-post.php';
+		// require_once plugin_dir_path( __FILE__ ) . 'includes/class-mf2-post.php';
 
 	}
 	add_action( 'plugins_loaded', 'parse_this_loader', 11 );
 }
+
+// autoloader
+spl_autoload_register(
+	function ( $class ) {
+		$base_dir = __DIR__ . '/includes/';
+		if ( strncmp( $class, 'Parse_This', strlen( 'Parse_This' ) ) !== 0 ) {
+			return;
+		}
+		$filename = 'class-' . strtolower( str_replace( '_', '-', $class ) );
+		$file     = $base_dir . $filename . '.php';
+		if ( file_exists( $file ) ) {
+			require $file;
+		}
+	}
+);
