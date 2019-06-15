e4a4af206fbbe5526e144f4649f7932ac4203b5e
David Shanske <david@shanske.com>
2019-06-15 05:40:35 +0000

    Switch autoloader to separate file

--- parse-this.php
+++ parse-this.php
@@ -16,6 +16,8 @@
 
 if ( ! function_exists( 'parse_this_loader' ) ) {
 	function parse_this_loader() {
+		require_once plugin_dir_path( __FILE__ ) . 'includes/autoload.php';
+
 		require_once plugin_dir_path( __FILE__ ) . 'includes/functions.php';
 		// Parse This REST Endpoint
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-rest-parse-this.php';
@@ -24,19 +26,3 @@ if ( ! function_exists( 'parse_this_loader' ) ) {
 	add_action( 'plugins_loaded', 'parse_this_loader', 11 );
 }
 
-// autoloader
-spl_autoload_register(
-	function ( $class ) {
-		$base_dir = __DIR__ . '/includes/';
-		$bases = array( 'Parse_This', 'MF2' );
-		foreach( $bases as $base ) {
-			if ( strncmp( $class, $base, strlen( $base ) ) === 0 ) {
-				$filename = 'class-' . strtolower( str_replace( '_', '-', $class ) );
-				$file     = $base_dir . $filename . '.php';
-				if ( file_exists( $file ) ) {
-					require $file;
-				}
-			}
-		}
-	}
-);
