c9fc3f2e05482e52dcc0a27f7bf7f34139d2ebbc
David Shanske <david@shanske.com>
2020-01-29 20:36:51 +0000

    Change references to refs per advice that microsub uses that.

--- includes/functions.php
+++ includes/functions.php
@@ -77,21 +77,21 @@ if ( ! function_exists( 'jf2_references' ) ) {
 			}
 			// Indicates nested type
 			if ( array_key_exists( 'type', $value ) && 'cite' === $value['type'] ) {
-				if ( ! isset( $data['references'] ) ) {
-					$data['references'] = array();
+				if ( ! isset( $data['refs'] ) ) {
+					$data['refs'] = array();
 				}
 				if ( isset( $value['url'] ) ) {
-					$data['references'][ $value['url'] ] = $value;
-					$data[ $key ]                        = array( $value['url'] );
+					$data['refs'][ $value['url'] ] = $value;
+					$data[ $key ]                  = array( $value['url'] );
 				}
 			}
 			if ( 'category' === $key && array_key_exists( 'type', $value ) && 'card' === $value['type'] ) {
-				if ( ! isset( $data['references'] ) ) {
-					$data['references'] = array();
+				if ( ! isset( $data['refs'] ) ) {
+					$data['refs'] = array();
 				}
 				if ( isset( $value['url'] ) ) {
-					$data['references'][ $value['url'] ] = $value;
-					$data[ $key ]                        = array( $value['url'] );
+					$data['refs'][ $value['url'] ] = $value;
+					$data[ $key ]                  = array( $value['url'] );
 				}
 			}
 		}
